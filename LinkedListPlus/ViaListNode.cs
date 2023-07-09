c465c69df9c31fa49fefe088c3246b9c653ff601
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-07-09 15:09:04 +0300

    ViaListNode oluşturuldu

---
+++ LinkedListPlus/ViaListNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListPlus
+{
+    public class ViaListNode<T> 
+    {
+        public ViaListNode(T value)
+        {
+            Value = value;
+        }
+
+        public ViaListNode<T> Next { get; set; }
+        public ViaListNode<T> Back { get; set; }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Value}";
+        }
+    }
+}
