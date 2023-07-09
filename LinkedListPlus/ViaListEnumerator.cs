9f75436ed14b560abd0d52f62fa401c05463bdb1
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-07-09 17:10:43 +0300

    IEnumerator<T> yazıldı

---
+++ LinkedListPlus/ViaListEnumerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace LinkedListPlus
+{
+    public class ViaListEnumerator<T> : IEnumerator<T>
+    {
+        private ViaListNode<T> Head;
+        private ViaListNode<T> _current;
+
+        public ViaListEnumerator(ViaListNode<T> head)
+        {
+            Head = head;
+            _current = null;
+        }
+
+        public T Current => _current.Value;
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            Head = null; //head i kaybederisek silinmiiş olur
+        }
+
+        public bool MoveNext()
+        {
+            if (_current == null) //current boş ise uygulama daha yeni başlamış demektir head i ata
+            {
+                _current = Head;
+                return true;
+            }
+            else
+            {
+                _current = _current.Next;
+                return _current != null ? true : false;  //Eleman var ise true dön yoksa false dön
+            }
+        }
+
+        public void Reset()
+        {
+            _current = null;  //resetlemek için current null olmalıdır MoneNext() de en baştan başlasın diye
+        }
+    }
+}
--- LinkedListPlus/ViaList_Sadık.cs
+++ LinkedListPlus/ViaList_Sadık.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,17 @@ using System.Threading.Tasks;
 
 namespace LinkedListPlus
 {
-    public partial class ViaList<T>
+    public partial class ViaList<T> : IEnumerable<T>
     {
-       //Methot1
+        //Methot1
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new ViaListEnumerator<T>(Head);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
