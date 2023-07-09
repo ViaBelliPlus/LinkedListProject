ff2029c656fc878d4b9c93a5795a185c630e8677
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-07-09 17:19:35 +0300

    .

--- LinkedListPlus/ViaList_Sadık.cs
+++ LinkedListPlus/ViaList_Sadık.cs
@@ -9,7 +9,26 @@ namespace LinkedListPlus
 {
     public partial class ViaList<T> : IEnumerable<T>
     {
-        
+        public ViaList(IEnumerable<T> collection)
+        {
+            //ilk tanımlanma ısrasında hazır bir listeyi bu listeye ekleme işlemini yapar
+            foreach (var item in collection)
+            {
+                //this.Add(item);
+                throw new NotImplementedException();
+            }
+        }
+
+        public T Removelast()
+        {
+            throw new NotImplementedException();
+        }
+        public T Remove()
+        {
+            throw new NotImplementedException();
+        }
+       
+
         public IEnumerator<T> GetEnumerator()
         {
             return new ViaListEnumerator<T>(Head);
