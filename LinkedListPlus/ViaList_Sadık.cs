9544a53dca70646f53e6716f5329bf879b26f4f0
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-07-09 22:17:11 +0300

    eksikler tamamlandı

--- LinkedListPlus/ViaList_Sadık.cs
+++ LinkedListPlus/ViaList_Sadık.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,122 +26,165 @@ namespace LinkedListPlus
 
         public T RemoveFirst()
         {
-            var value = Head.Value; //degeri dönmek için tutuk
-            Head.Next.Back = null; // []->[]  2. degerın öncesini null yaptık 
-            Head = Head.Next; //yeni başlangiç değerimiz bir sonraki
-            count--;
-            return value;
+            var value = Head.Value; // Değeri dönmek için geçici bir değişkende sakla
+            if (Head != null) // Liste boş değilse devam et
+            {
+                if (Head.Next != null) // Başka bir öğe varsa
+                {
+                    Head.Next.Back = null; // İkinci öğenin öncesini null yap
+                    Head = Head.Next; // Yeni başlangıç değerimizi ikinci öğe olarak güncelle
+                }
+                else // Sadece bir öğe varsa
+                {
+                    Head = null; // Listeyi boşalt
+                }
+                count--;
+            }
+            return value; // Kaldırılan değeri dön
         }
 
         public T this[int index]
         {
+            
             get
             {
-                var value= Head;
-                for (int i = 0; i < index; i++)
+                var value = Head;
+                if (count<=index)
                 {
-                    value = value.Next;
+                    for (int i = 0; i < index; i++)
+                    {
+                        value = value.Next;
+                    }
+                    return value.Value;
                 }
-                return value.Value;
+                else
+                {
+                    throw new Exception();
+                }
             }
             set
             {
-                var data = Head;
-                for (int i = 0; i < index; i++)
+                if (count <= index)
                 {
-                    data = data.Next;
+                    var data = Head;
+                    for (int i = 0; i < index; i++)
+                    {
+                        data = data.Next;
+                    }
+                    data.Value = value;
                 }
-                data.Value = value;
+                else
+                {
+                    throw new Exception();
+                }
+                
             }
 
         }
 
-        public void RemoveAfter(ViaListNode<T> node)// []->[]->[]
+        public void RemoveAfter(ViaListNode<T> node)
         {
+            // Belirtilen düğümden sonraki düğümü kaldırma işlemi
+            var delete = node.Next;
+            // İlk olarak, düğümün null olup olmadığını kontrol ediyoruz.
             if (node == null)
             {
-                throw new ArgumentException("Item must not be null");
+                throw new ArgumentNullException(nameof(node), "Düğüm null olamaz.");
             }
-            if (Head == null)
+            if (node==Head)
             {
-                throw new NullReferenceException();
-            }
-            if (Head == Tail)
-            {
-                throw new Exception("diniz yeterli büyüklikte değil");
-            }
-            if (Head == node)
-            {
-                throw new Exception("Baş kısımın sonrası yok tur");
-            }
-            else if (Tail == node)
-            {
-                Tail = Tail.Next;
-                Tail.Back = null;
+                Head.Next = Head.Next.Next;
+                Head.Next.Next.Back = Head;
+                disconnection(delete);
                 count--;
                 return;
             }
-            else if (node.Next.Next == null)
+            if (node==Tail)
             {
-                throw new ArgumentNullException();
+                return; //sonrası yok 
             }
-            node.Next = node.Next.Next;
-            node.Next.Next.Back = node.Next;
+            if (node==Tail.Back)
+            {
+                Tail = Tail.Back;
+                disconnection(delete);
+                count--;
+                return;
+            }
+            node.Next.Next.Back = node;
+            node.Next=node.Next.Next;
+
             count--;
             return;
+
         }
+
+
 
         public T RemoveLast()
         {
-            var value = Tail.Value;//degeri dönmek için tutuk
-            Tail.Back.Next = null;// []<-[] sondan 1 öncekinin sonrasını null yaptık 
-            Tail = Tail.Back; //yeni tail bi önceki yapıldı
+            if (Tail == null)
+            {
+                throw new NullReferenceException("Listede eleman bulunmuyor.");
+            }
+
+            var value = Tail.Value; // Silinen düğümün değerini saklamak için bir değişkende tutarız
+
+            // Listenin sadece bir düğümü varsa, Head ve Tail'e null atanır
+            if (Head == Tail)
+            {
+                Head = null;
+                Tail = null;
+            }
+            else
+            {
+                Tail.Back.Next = null; // Sondan bir önceki düğümün sonraki referansını null yaparız
+                Tail = Tail.Back; // Yeni tail, bir önceki düğüm olur
+            }
+
             count--;
-            return value;
+            return value; // Saklanan değeri döndürürüz
         }
+
 
         public void RemoveBefore(ViaListNode<T> node)
         {
+            // Belirtilen düğümden sonraki düğümü kaldırma işlemi
+            var delete = node.Next;
+            // İlk olarak, düğümün null olup olmadığını kontrol ediyoruz.
             if (node == null)
             {
-                throw new ArgumentException("Item must not be null");
+                throw new ArgumentNullException(nameof(node), "Düğüm null olamaz.");
             }
-            if (Head == null)
+            if (node == Head)
             {
-                throw new NullReferenceException();
+                Head.Back = Head.Back.Back;
+                Head.Back.Back.Next = Head;
+                disconnection(delete);
+                count--;
+                return;
             }
-            if (Head == Tail)
+            if (node == Tail)
             {
-                throw new Exception("diniz yeterli büyüklikte değil");
+                return; //sonrası yok 
             }
-            if (Head == node)
+            if (node == Tail.Next)
             {
-                Head = Head.Back;
-                Head.Next = null;
+                Tail = Tail.Next;
+                disconnection(delete);
+                count--;
+                return;
             }
-            else if (Tail == node)
-            {
-                throw new Exception("Baş kısımın öncesi yok tur");
-            }
-            else if (node.Back.Back == null)
-            {
-                throw new ArgumentNullException();
-            }
+            node.Back.Back.Next = node;
             node.Back = node.Back.Back;
-            node.Back.Back.Next = node.Back;
+
             count--;
             return;
         }
 
-        public ViaListNode<T> RemoveAt(T value)
+        public void disconnection(ViaListNode<T> node)
         {
-            if (value == null)
-            {
-                throw new ArgumentException("Item must not be null");
-            }
-            var respons = SerchNode(value);
-            count--;
-            return respons;
+            node.Next = null;
+            node.Back = null;
         }
 
         public void RemoveAt(ViaListNode<T> node)
@@ -174,15 +218,16 @@ namespace LinkedListPlus
             return;
         }
 
-        public T RemoveAtValue(T value)
+        public void RemoveAtValue(T value)
         {
             if (value == null)
             {
                 throw new ArgumentException("Item must not be null");
             }
             var respons = SerchNode(value);
+            RemoveAt(respons);
             count--;
-            return respons.Value;
+            
         }
 
         public ViaListNode<T> SerchNode(T value)
